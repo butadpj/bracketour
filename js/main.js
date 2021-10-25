b19b0f53d894eb98b955daed1ba726529eb9637c
butadpj <butadpj@gmail.com>
2021-10-25 08:07:34 +0800

    Refactor input validation logic

--- js/main.js
+++ js/main.js
@@ -1,72 +1,106 @@
-import { regexValidate, addClassTo } from './utils/index.js';
+import { regexPattern, addClassTo } from './utils/index.js';
 
+// SELECTORS
 const participantInput = document.getElementById('participant-input');
 
-const minElement = document.querySelector('.constraints .min');
-const maxElement = document.querySelector('.constraints .max'); 
+const minInputElement = document.querySelector('.constraints .min');
+const maxInputElement = document.querySelector('.constraints .max');
 
 const submitBtn = document.getElementById('submit-btn');
+//
 
-const numbersOnly = regexValidate('number');
+// GLOBALS/CONSTANTS
+const MAX_PARTICIPANTS = 50;
+const MIN_PARTICIPANTS = 2;
+const numberPattern = regexPattern.number;
+//
 
-participantInput.addEventListener('input', (e) => {
-  const input = e.target.value;
+// FUNCTIONS
+const validateInputWithPattern = (input, pattern) => {
+  if (
+    input && // not empty
+    pattern.test(input) && // valid number
+    Number(input) <= MAX_PARTICIPANTS &&
+    Number(input) >= MIN_PARTICIPANTS
+  )
+    return {
+      valid: true,
+      value: input,
+    };
 
-  const maxPraticipants = 50;
-  const minPraticipants = 2;
-
-  const validateInput = numbersOnly(pattern => {
-    if (
-        input && // not empty
-        pattern.test(input) && // valid number
-        Number(input) <= maxPraticipants &&
-        Number(input) >= minPraticipants
-      ) { 
-        return {
-          valid: true,
-          value: input
-        }
-    } 
-
-    if (Number(input) > maxPraticipants) return {
-      valid: false,
-      error: 'max'
-    }
-
-    if (Number(input) < minPraticipants) return {
-      valid: false,
-      error: 'min'
-    }
-
-    // default
+  if (Number(input) > MAX_PARTICIPANTS)
     return {
       valid: false,
-      error: 'Please input a valid number',
+      error: 'max',
+    };
+
+  if (Number(input) < MIN_PARTICIPANTS)
+    return {
+      valid: false,
+      error: 'min',
+    };
+
+  // default
+  return {
+    valid: false,
+    error: 'Please input a valid number',
+  };
+};
+
+const invalidInputAndButton = (inputElement, buttonElement) => {
+  inputElement.classList.add('danger');
+  inputElement.classList.remove('success');
+
+  buttonElement.classList.add('disabled');
+  buttonElement.disabled = true;
+};
+
+const validInputAndButton = (inputElement, buttonElement) => {
+  inputElement.classList.add('success');
+  inputElement.classList.remove('danger');
+
+  buttonElement.classList.remove('disabled');
+  buttonElement.disabled = false;
+};
+//
+
+// DOM nodes needed to be loaded first
+// For testing purposes
+window.addEventListener('load', () => {
+  maxInputElement.querySelector('span').textContent = MAX_PARTICIPANTS;
+  minInputElement.querySelector('span').textContent = MIN_PARTICIPANTS;
+
+  // EVENT LISTENERS
+  participantInput.addEventListener('input', (e) => {
+    const input = e.target.value;
+
+    const inputResult = validateInputWithPattern(input, numberPattern);
+
+    if (!inputResult.valid) {
+      invalidInputAndButton(participantInput, submitBtn);
+
+      if (inputResult.error === 'min') {
+        const removeAfterSecond = addClassTo('apply-shake', minInputElement);
+        removeAfterSecond(1);
+      }
+      if (inputResult.error === 'max') {
+        const removeAfterSecond = addClassTo('apply-shake', maxInputElement);
+        removeAfterSecond(1);
+      }
+      return;
     }
+
+    validInputAndButton(participantInput, submitBtn);
   });
-
-  if (!validateInput.valid) {
-    participantInput.classList.add('danger');
-    participantInput.classList.remove('success');
-    submitBtn.classList.add('disabled');
-
-    submitBtn.disabled = true;
-
-    if (validateInput.error === 'min') {
-      const removeAfterSecond = addClassTo('apply-shake', minElement);
-      removeAfterSecond(1);
-    }
-    if (validateInput.error === 'max') {
-      const removeAfterSecond = addClassTo('apply-shake', maxElement);
-      removeAfterSecond(1);
-    }
-    return
-  } 
-
-  participantInput.classList.add('success');
-  participantInput.classList.remove('danger');  
-  submitBtn.classList.remove('disabled');
-
-  submitBtn.disabled = false;
+  //
 });
 
+// EXPOSE
+export {
+  validateInputWithPattern,
+  validInputAndButton,
+  invalidInputAndButton,
+  numberPattern,
+  MAX_PARTICIPANTS,
+  MIN_PARTICIPANTS,
+};
